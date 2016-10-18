23129c087417576262d433ed7e4a0a3740d84dbc
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-18 17:02:56 -0400

    created deal_game method and tested

--- standard_playing_cards/blackjack.rb
+++ standard_playing_cards/blackjack.rb
@@ -1,12 +1,22 @@
 module StandardPlayingCards
   class Blackjack
 
-    attr_accessor :game_deck, :dealer_hand, :player_hand
+    attr_accessor :game_deck, :dealer_hand, :player_hand, :num_players
 
     def initialize
       @game_deck = Deck.new
       @dealer_hand = []
       @player_hand = []
+      @num_players = 2
+    end
+
+    def deal_game!
+      game_deck.shuffle_deck!
+      counter = num_players
+      counter.times do
+        player_hand << game_deck.deal_card!
+        dealer_hand << game_deck.deal_card!
+      end
     end
 
   end
--- test/blackjack_test.rb
+++ test/blackjack_test.rb
@@ -12,4 +12,28 @@ class BlackjackTest < Minitest::Test
     assert blackjack.game_deck.is_a?(StandardPlayingCards::Deck)
   end
 
+  def test_deal_two_cards_player
+    new_blackjack = StandardPlayingCards::Blackjack.new
+    new_blackjack.deal_game!
+    assert_equal 2, new_blackjack.player_hand.size
+  end
+
+  def test_deal_two_cards_dealer
+    new_blackjack = StandardPlayingCards::Blackjack.new
+    new_blackjack.deal_game!
+    assert_equal 2, new_blackjack.dealer_hand.size
+  end
+
+  def test_player_hand_card
+    new_blackjack = StandardPlayingCards::Blackjack.new
+    new_blackjack.deal_game!
+    assert new_blackjack.player_hand[0].is_a?(StandardPlayingCards::Card)
+  end
+
+  def test_dealer_hand_card
+    new_blackjack = StandardPlayingCards::Blackjack.new
+    new_blackjack.deal_game!
+    assert new_blackjack.dealer_hand[0].is_a?(StandardPlayingCards::Card)
+  end
+
 end
