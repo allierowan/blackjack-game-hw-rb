2bf74ad614a710845f7f91a4b4a73b6c919890d5
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-18 15:13:26 -0400

    created standard_playing_cards dir and module. put deck inside of module. created invalid_card_input_error

---
+++ invalid_card_input_error.rb
@@ -0,0 +1,2 @@
+class InvalidCardInputError < StandardError
+end
---
+++ standard_playing_cards/deck.rb
@@ -0,0 +1,35 @@
+require "./card"
+module StandardPlayingCards
+  class Deck
+
+    attr_accessor :deck_cards
+
+    def initialize(input_cards = [])
+      if input_cards.length > 0
+        @deck_cards = input_cards
+      else
+        @deck_cards = []
+          FACE_VALUES.each do |value|
+          SUIT_VALUES.each do |suit|
+            deck_cards << Card.new(value, suit)
+          end
+        end
+      end
+    end
+
+    def shuffle_deck!
+      raise DeckEmptyError if deck_cards.size ==0
+      deck_cards.shuffle!
+    end
+
+    def deal_card!
+      raise DeckEmptyError if deck_cards.size ==0
+      deck_cards.shift
+    end
+
+    def empty?
+      deck_cards == []
+    end
+
+  end
+end
