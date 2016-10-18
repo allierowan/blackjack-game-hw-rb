c109c7825b6120a274cac7c07810b67541da737f
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-18 15:14:42 -0400

    put card class inside standardplayingcards module and moved fiel

---
+++ standard_playing_cards/card.rb
@@ -0,0 +1,16 @@
+module StandardPlayingCards
+  class Card
+
+    attr_reader :face_value, :suit
+    def initialize(face_value, suit)
+
+      @face_value = face_value
+      @suit = suit
+    end
+
+    def ==(other_card)
+      face_value == other_card.face_value && suit == other_card.suit
+    end
+
+  end
+end
