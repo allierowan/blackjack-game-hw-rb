50ca6dbcc6d250789f983f0d0d288906a7e5ee67
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-18 13:53:09 -0400

    tested for the correct suits and values to be present in a deck

--- deck_test.rb
+++ deck_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative './deck'
+require 'set'
 
 class DeckTest <Minitest::Test
   def deck
@@ -13,5 +14,21 @@ class DeckTest <Minitest::Test
 
   def test_b_52_cards
     assert_equal 52, deck.deck_cards.size
+  end
+
+  def test_c_has_13_values
+    value_set = Set.new
+    deck.deck_cards.each do |card|
+      value_set << card.face_value
+    end
+    assert_equal 13, value_set.size
+  end
+
+  def test_d_has_4_suits
+    suit_set = Set.new
+    deck.deck_cards.each do |card|
+      suit_set << card.suit
+    end
+    assert_equal Set["Diamonds", "Hearts", "Clubs", "Spades"], suit_set
   end
 end
