835d781c6a98016d132ac0e4d55d32ffa5ee8d0e
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-19 22:37:36 -0400

    allowed user to play again.

--- test/game.rb
+++ test/game.rb
@@ -1,30 +1,38 @@
 require_relative '../standard_playing_cards'
 
-curr_game = StandardPlayingCards::Blackjack.new
-curr_game.deal_game!
+play_again = true
 
-puts "Welcome to Blackjack. The dealer has dealt the game"
-puts "\nYour hand is:"
-curr_game.player_hand.show_player_hand
-puts "\nThe dealer's upcard is:"
-curr_game.dealer_hand.show_dealer_hand
+until !play_again
+  curr_game = StandardPlayingCards::Blackjack.new
+  curr_game.deal_game!
 
-until curr_game.game_over?
-  if curr_game.player_last_move != "Stand"
-    puts "Would you like to Hit or Stand? (Hit/Stand)"
-    input = gets.chomp
-    curr_game.player_turn(input)
+  puts "Welcome to Blackjack. The dealer has dealt the game"
+  puts "\nYour hand is:"
+  curr_game.player_hand.show_player_hand
 
-    puts "Thank you. Your hand is now:"
-    curr_game.player_hand.show_player_hand
+  puts "\nThe dealer's upcard is:"
+  curr_game.dealer_hand.show_dealer_hand
+
+  until curr_game.game_over?
+    if curr_game.player_last_move != "Stand"
+      puts "Would you like to Hit or Stand? (Hit/Stand)"
+      input = gets.chomp
+      curr_game.player_turn(input)
+
+      puts "Thank you. Your hand is now:"
+      curr_game.player_hand.show_player_hand
+    end
+
+    if !curr_game.game_over?
+      curr_game.dealer_turn!
+      dealer_move = curr_game.dealer_last_move
+      puts "Now the dealer's turn. The dealer's move was: #{dealer_move}"
+    end
   end
 
-  if !curr_game.game_over?
-    curr_game.dealer_turn!
-    dealer_move = curr_game.dealer_last_move
-    puts "Now the dealer's turn. The dealer's move was: #{dealer_move}"
-  end
+  puts "#{curr_game.game_winner} won!"
+  puts curr_game.game_over_reason
+  puts "Would you like to play again? Y/N"
+  input = gets.chomp
+  play_again = false unless input == "Y"
 end
-
-puts "#{curr_game.game_winner} won!"
-puts curr_game.game_over_reason
