df37bb82512716b3dfe8affb4032145554bfc0be
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-19 20:58:54 -0400

    created hand size test

--- test/hand_test.rb
+++ test/hand_test.rb
@@ -3,5 +3,12 @@ require 'minitest/pride'
 require_relative '../standard_playing_cards'
 
 class HandTest <Minitest::Test
-  
+
+  def test_size_of_hand
+    ace_diamonds = StandardPlayingCards::Card.new("A", "Diamonds")
+    four_spades = StandardPlayingCards::Card.new(4, "Spades")
+    my_hand = StandardPlayingCards::Hand.new([ace_diamonds, four_spades])
+    assert_equal 2, my_hand.hand_size
+  end
+
 end
