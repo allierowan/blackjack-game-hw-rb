64567040b96ee45ee5c4b3510172a249373998f3
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-19 22:30:01 -0400

    fixed game logic so that if a player stands, they aren't asked for they're move again

--- test/game.rb
+++ test/game.rb
@@ -10,12 +10,14 @@ puts "\nThe dealer's upcard is:"
 curr_game.dealer_hand.show_dealer_hand
 
 until curr_game.game_over?
-  puts "Would you like to Hit or Stand? (Hit/Stand)"
-  input = gets.chomp
-  curr_game.player_turn(input)
-  
-  puts "Thank you. Your hand is now:"
-  curr_game.player_hand.show_player_hand
+  if curr_game.player_last_move != "Stand"
+    puts "Would you like to Hit or Stand? (Hit/Stand)"
+    input = gets.chomp
+    curr_game.player_turn(input)
+
+    puts "Thank you. Your hand is now:"
+    curr_game.player_hand.show_player_hand
+  end
 
   if !curr_game.game_over?
     curr_game.dealer_turn!
